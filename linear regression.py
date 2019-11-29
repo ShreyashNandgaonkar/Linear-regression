cf8020c1965f5255039839413a4fcf9c47e50a44
ShreyashNandgaonkar <46418251+ShreyashNandgaonkar@users.noreply.github.com>
2019-11-29 21:50:42 +0530

    Update linear regression.py

--- linear regression.py
+++ linear regression.py
@@ -1,19 +1,26 @@
 #Linear regression using sklearn
 from sklearn import linear_model
 from sklearn import datasets
+import seaborn as sns
+import matplotlib.pyplot as plt
 #load dataset
+
 d= datasets.load_boston()
-#check the data
+#dataexploration
 d
 #import packages
 import numpy as np
 import pandas as pd
 #define predictors and load dataset in pandas dataframe
 df = pd.DataFrame(d.data,columns=d.feature_names)
+df.head()
+df.info()
+df.describe()
+sns.pairplot(df)
 #putting target variable(MEDV) in another dataframe
 t = pd.DataFrame(d.target,columns=["MEDV"])
 # predictor(dependant) is df and Target(independant) is t
-#fdefine X and y for our model
+#define X and y for our model
 X= df
 y=t["MEDV"]
 #Model
